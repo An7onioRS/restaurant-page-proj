50a329bbd89adf00fb8883342cbe1f883c8b55f1
Antonio Simeonov <antoniosimeonov7@gmail.com>
2022-10-31 08:32:14 +0200

    Fix images not showing up

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # restaurant-page-proj
+
 A responsive restaurant home page project to practice plain CSS and JavaSCript for web development and npm and webpack to create modules
--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
-import './css/style.css'
+import "./css/style.css";
+import "../../images/";
 
-let menu = document.querySelector('#menu-bar')
-let navbar = document.querySelector('.navbar')
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
 
-menu.addEventListener('click', () => {
-    menu.classList.toggle('fa-times')
-    navbar.classList.toggle('active')
-})
+menu.addEventListener("click", () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+});
 
-window.addEventListener('scroll', () => {
-    menu.classList.remove('fa-times')
-    navbar.classList.remove('active')
-    
-    if (window.scrollY > 60) {
-        document.querySelector('#scroll-top').classList.add('active')     
-    } else {
-        document.querySelector('#scroll-top').classList.remove('active')
-    }
-})
+window.addEventListener("scroll", () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+
+  if (window.scrollY > 60) {
+    document.querySelector("#scroll-top").classList.add("active");
+  } else {
+    document.querySelector("#scroll-top").classList.remove("active");
+  }
+});
 
 function loader() {
-    document.querySelector('.loader-container').classList.add('fade-out')
+  document.querySelector(".loader-container").classList.add("fade-out");
 }
 
 function fadeOut() {
-    setInterval(loader, 3000) 
+  setInterval(loader, 3000);
 }
 
-window.onload = fadeOut()   
+window.onload = fadeOut();
